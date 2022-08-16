4a242c52652d425ebbea61280f9b1d849a4f3305
Stanislav Lihodievski <85512617+Stanislav-Lihod@users.noreply.github.com>
2022-08-16 11:21:41 +0400

    add comments

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
-const btn = document.querySelector('.btn.btn-danger')
-let counter = 0
+const btn = document.querySelector('.btn.btn-danger') // Выбор кнопки на которую кликать
+let counter = 0 // Счетчик для проверки наличия дорогих билетов
 
 const checkFreeTickets = () =>{
-  const alertInfo = document.querySelector('.alert-info.ng-hide')
-  const tickets = document.querySelectorAll('tr.ng-scope')
-  if (alertInfo) {
-
-    if (tickets.length > 0) {
+  const alertInfo = document.querySelector('.alert-info.ng-hide') // Проверка скрыто ли уведомление об отсутствии билетов
+  const tickets = document.querySelectorAll('tr.ng-scope') // Массив билетов на эту дату
+  if (alertInfo) { // Если уведомления нет то
+    if (tickets.length > 0) { // Проверка на наличие билетов в массиве
       tickets.forEach(ticket=>{
-        const column = ticket.querySelectorAll('td.ng-binding')
-        const price = column[column.length-1]
+        const column = ticket.querySelectorAll('td.ng-binding') // Выбираем все колонки в каждом билете
+        const price = column[column.length-1] // Вбираем последнюю колонку с ценой
   
-        if (price.textContent.includes('35')) {
+//Ниже делаем проверку на стоимость билета
+
+        if (price.textContent.includes('35')) { // Укажите стоимость дешевого билета на данный момент
           console.log('Найден билет за 35, хватай его')
           let audio = new Audio();
           audio.src = 'https://bigsoundbank.com/UPLOAD/wav/2374.wav';
           audio.autoplay = true;
-          clearInterval(setIntervalId)
-        } else if (counter === 0){
+          clearInterval(setIntervalId) // Если билет дешевый скрипт остановится
+        } else if (counter === 0){ // Если билет дорогой то один раз придет уведомление но скрипт продолжит кликать по кнопке.
           counter = 1
           console.log('Найден дорогой билет')
           let audio = new Audio();
@@ -25,11 +26,11 @@ const checkFreeTickets = () =>{
           audio.autoplay = true;
         } else{
           btn.click()
-          console.log('Я работаю после находки билета')
+          console.log('Я работаю после нахождения билета')
         }  
       })    
     }
-  }else{
+  }else{ // Если билеты разобрали то таблица очистится и скрипт вернется в начальную форму обнулив счетчик дорогих билетов.
     const table = document.querySelector('tbody.table-hover')
     tickets.forEach(ticket=>{
       ticket && table.removeChild(ticket)
